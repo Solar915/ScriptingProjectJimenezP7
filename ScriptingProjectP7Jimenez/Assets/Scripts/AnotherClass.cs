cbaf48934867c0c99ffaab6a061f47dfc3c66751
Ethan Jimenez <723068@student.dusd.net>
2023-11-30 14:52:54 -0800

    I have created 2 scripts for my scripting project
    For my scripting project, I have created 2  new scripts named AnotherClass and ScopeAndAccess

---
+++ ScriptingProjectP7Jimenez/Assets/Scripts/AnotherClass.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Profiling;
+using UnityEngine;
+
+public class AnotherClass : MonoBehaviour
+{
+    public int apples;
+    public int bananas;
+
+
+    private int stapler;
+    private int sellotape;
+
+
+    public void FruitMachine(int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Fruit total: " + answer);
+    }
+
+
+    private void OfficerSort(int a, int b)
+    {
+        int answer;
+        answer = a + b;
+        Debug.Log("Officer Supplies total: " + answer);
+    }
+}
+    
+
+   
